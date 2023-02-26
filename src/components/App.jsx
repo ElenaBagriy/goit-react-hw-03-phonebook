f46695d80588fa1aba4e9fcfa1d397bb3e11b38f
ElenaBagriy <elena.bagriy1989@gmail.com>
2023-02-26 16:47:19 +0100

    finally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,7 +58,7 @@ class App extends Component {
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
     }
